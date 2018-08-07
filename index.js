707531f95adc1f95350f968a2ee6add50a8373e2
nm777taka <nm777.taka.dev@gmail.com>
2018-08-07 13:59:23 +0900

    main画面修正

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { h, app } from "hyperapp"
 import Twitter from "twitter"
+import {Card} from "./card";
 
 const state = {
+    drawList: [1,2,3,4,5,6,7,8,9,10]
 }
 
 const actions = {
+
 }
 
 const consumer_key = 'MUxI500CBeRDCYBjIk0ZZnA0M'
@@ -22,47 +25,62 @@ const twitterClient = new Twitter({
 
 
 const view = (state, actions) => (
-    <div className={"ui container"}>
-        <div className="ui three item menu">
-            <a className="active item">page1</a>
-            <a className="item">page2</a>
-            <a className="item">page3</a>
-        </div>
+    <div>
+        <header className={"nav"}>
+            <div className={"nav-left"}>
+                <span className={"nav-item"}>
+                    <span className={"fa fa-language"}></span>OneDraw
+                </span>
+            </div>
+            <div className={"nav-right"}>
+                <span className={"nav-item"}>
+                    <a href="#" className={"fa fa-user"}>ログイン</a>
+                </span>
+            </div>
+        </header>
 
-        <div className="ui three column grid">
-            <div className="column">
-                <div className="ui fluid card">
-                    <div className="image">
-                        <img src="/images/sample1.jpg" />
-                    </div>
-                    <div className="content">
-                        <a className="header">Daniel Louise</a>
-                    </div>
-                </div>
-            </div>
-            <div className="column">
-                <div className="ui fluid card">
-                    <div className="image">
-                        <img src="/images/sample2.jpg" />
-                    </div>
-                    <div className="content">
-                        <a className="header">Helen Troy</a>
-                    </div>
-                </div>
-            </div>
-            <div className="column">
-                <div className="ui fluid card">
-                    <div className="image">
-                        <img src="/images/sample3.png" />
-                    </div>
-                    <div className="content">
-                        <a className="header">Elliot Fu</a>
-                    </div>
+        <div className={"hero is-info is-bold"}>
+            <div className={"hero-body"}>
+                <div className={"container"}>
+                    <h1 className={"title"}>OneDraw</h1>
+                    <h2 className={"subtitle"}>ワンドロランキング</h2>
                 </div>
             </div>
         </div>
 
+        <section className={"level"}>
+            <div className={"level-left"}></div>
+            <div className={"level-right"}>
+                <div className={"level-item tabs"}>
+                    <ul>
+                        <li className={"is-active"}><a>最近の投稿</a></li>
+                        <li><a>ランキング</a></li>
+                        <li><a>人気絵師</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className={"level-right"}>
+                <div className={"level-item"}>
+                    <a className={"button is-greylight"}>フォロー</a>
+                </div>
+            </div>
+        </section>
+
+        <section className="section cards">
+            <div className="container">
+                <div className="columns is-multiline">
+                    {state.drawList.map((item) => {
+                        return (
+                            <div className="column">
+                                {Card()}
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
     </div>
+
 )
 
 const main = app(state, actions, view, document.body)
